e799ad358b8587916026af9d9d9863cf96f014a0
Francisco Pereira <franciscopere987@gmail.com>
2025-02-21 15:53:06 -0300

    ADD: Some styling and new build

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
-import './assets/main.css'
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    theme: {
+        themes: {
+            light: {
+                colors: {
+                    secondary: '#7E52A0',
+                    primary: '#29274C',
+                    background: '#012A36',
+                }
+
+            }
+        }
+    },
+    components,
+    directives
+})
+
 const app = createApp(App)
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,13 +1,49 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 
 </script>
 
 
 <template>
-    <main>
-        <div>
-            Work in progress...
+    <div class="main-background bg-background">
+        <div class="menu-card bg-primary" >
+            <v-card class="bg-secondary text-center" text="About" variant="tonal"></v-card>
+            <v-card class="bg-primary text-center" text="Blog" variant="tonal"></v-card>         
         </div> 
-    </main>
+        <div class="in-progress-container">
+            <v-card class="work-in-progress-card bg-primary">
+                <v-card-title class="text-center">Work In Progress...</v-card-title>
+            </v-card>
+        </div>
+    </div>
 </template>
+
+
+<style>
+ .menu-card {
+   flex: 1;
+   max-width: 10vw;
+   min-height: 100vh;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-evenly;
+   position: static;
+}
+
+ .work-in-progress-card {
+    height: 50vh;
+    width: 40vw;
+    align-content: center;
+}
+ .in-progress-container {
+    flex: 9;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+ .main-background {
+    display: flex;
+    flex-direction: row;
+    min-height: 100vh;
+ }
+</style>
